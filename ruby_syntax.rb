cfe3b6444dbf5128c6b93c99641ba510a2ff4541
mo <mojangles32@gmail.com>
2023-06-20 22:14:27 -0400

    New string section

--- ruby_syntax.rb
+++ ruby_syntax.rb
@@ -6,3 +6,39 @@ This is how we make a multiline comment
 puts 3.class 
 puts "HelloWorld, this is my first ruby program"
 puts "Hello".method(:class).class
+
+# If you want to get user input, you would use the gets.chomp method,
+# but you need to use the gets.chomp.to_i to get user input for numbers.
+
+# print "Type in your name: "
+# name = gets.chomp
+# print "Type in your ID number: "
+# id = gets.chomp.to_i
+
+# puts "Your name is #{name} and your ID number is: #{id}"
+
+first = "This is a string"
+second = "This is the second string"
+third = 123213
+puts "#{first}  #{second} #{third}"
+puts first.size
+
+#There are some basic string manipulation functions:
+first.upcase
+second.downcase
+puts first.upcase.reverse
+puts second.include? "This"
+
+#Concatenation can happen in 2 ways, one using the str.concat method and the other is using the << operator
+first.concat("Hi this is another one")
+second = second << "Adding something to the second one"
+puts first
+puts second
+
+# One interesting feature is the ability to freeze a string
+# This makes it so that you can no longer manipulate a string after it has been frozen
+
+# fourth = "Hi this string is going to freeze"
+# fourth.freeze
+# fourth.concat("Something new")
+# puts fourth

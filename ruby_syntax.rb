30a7cff21894f6e4463806ca595bceb89d1c89d3
mo <mojangles32@gmail.com>
2023-06-20 20:52:28 -0400

    some notes

---
+++ ruby_syntax.rb
@@ -0,0 +1,8 @@
+# This is how we do a single line comment
+=begin
+This is how we make a multiline comment
+=end
+
+puts 3.class 
+puts "HelloWorld, this is my first ruby program"
+puts "Hello".method(:class).class
